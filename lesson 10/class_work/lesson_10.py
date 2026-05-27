4721e7ad60cb6b93f82e4a65b2aeb11a30647f58
teklakapanadze0-dev <teklakapanadze0@gmail.com>
2026-05-27 18:56:03 +0400

    second commit

---
+++ lesson 10/class_work/lesson_10.py
@@ -0,0 +1,14 @@
+# 1)
+for i in range(1, 11):
+    print(i)
+
+for i in range(0, 101, 2):
+    print(i)
+# 2)
+
+name = input("enter your name: ")
+
+if name == "petre":
+    print("ola petre")
+else:
+    print("youre nor petre")
