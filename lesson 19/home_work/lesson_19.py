faa0b92b527a3552c51f501a4955d53ea74c4667
teklakapanadze0-dev <teklakapanadze0@gmail.com>
2026-06-25 23:10:35 +0400

    second commit

---
+++ lesson 19/home_work/lesson_19.py
@@ -0,0 +1,34 @@
+# 1) კომენტარებით ახსენით რა არის ფუნქცია
+# 2) კომენტარებით ახსენით როგორი ფუნქციაა print() (დღეს გაკვეთილზე ნასწავლიდან გამომდინარე)  
+# 3) კომენტარების სახით ახსენით თუ  რაარის find() და რაში ვიყენებთ მას
+# 4)  გააკეთეთ 3 მაგალითი find() ის გამოყენებით
+# 5) გააკეთეთ  2-2 მაგლითი .upper(), .lower(), .capitalize() ის გამოყენებით
+
+
+# 1)
+# ფუნქცია არის კოდი რომელიც რაღაც მოქმედებას ასრულებს
+# 2)ჩაშენებული ფუნქცია არის print()ი
+# 3)
+# find( )ს ვიყენებთ რომ ტექსტში გავიგოთ ესა და ეს სიტყვა რამდენჯერარი გამეორებვული
+# 4)
+text = "Hello World"
+print(text.find("World"))   
+
+text = "Python Programming"
+print(text.find("Pro")) 
+
+text = "Banana"
+print(text.find("na"))   
+
+# 5
+
+# print("mgeli".upper())
+# print("melakuda".upper())
+
+
+# print("CAT".lower())
+# print("DOG".lower())
+
+
+# print("hTMl".capitalize())
+# print("pYTHON".capitalize())
